12cb58b4dd51ca0bb0eab0e587674240d3361105
rodion <rodi-98@mail.ru>
2024-03-22 07:36:48 +0300

    Task 5,6

---
+++ recursive/Task 5,6/ComputeRecursive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace recursive.Task56
+{
+    public class Recursive_FifthTask
+    {
+        public static void printEvenValuesFromList(List<int> listOfnumbers)
+        {
+            if (listOfnumbers.Count() == 0)
+                return;
+
+            int number = listOfnumbers.First();
+            if (number % 2 == 0)
+                Console.WriteLine(number);
+
+            List<int> remainingList = listOfnumbers.GetRange(1, listOfnumbers.Count() - 1);
+
+            printEvenValuesFromList(remainingList);
+        }
+    }
+
+    public class Recursive_SixthTask
+    {
+        public static void printItemEvenIndexFromList(List<string> listOfElements)
+        {
+            doPrintItemEvenIndexFromList(listOfElements, 0);
+        }
+
+        private static void doPrintItemEvenIndexFromList(List<string> listOfElements, int currentIndex)
+        {
+            if (listOfElements.Count() <= currentIndex)
+                return;
+
+            Console.WriteLine(listOfElements.ElementAt(currentIndex));
+
+            doPrintItemEvenIndexFromList(listOfElements, currentIndex + 2);
+        }
+    }
+
+}
+
